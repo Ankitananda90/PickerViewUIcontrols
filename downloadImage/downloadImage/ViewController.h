f6cf4560fe880103562451a54faad1e190b04053
Ankita <ankitapriyadarsini8@gmail.com>
2016-05-15 13:03:06 -0400

    add downloadImage

---
+++ downloadImage/downloadImage/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  downloadImage
+//
+//  Created by Ankita on 5/2/16.
+//  Copyright © 2016 AnkitaApril. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+@property (weak, nonatomic) IBOutlet UIProgressView *progress;
+
+
+@end
+
---
+++ downloadImage/downloadImage/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  downloadImage
+//
+//  Created by Ankita on 5/2/16.
+//  Copyright © 2016 AnkitaApril. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()<NSURLSessionDelegate, NSURLSessionDownloadDelegate>
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration delegate:self delegateQueue:nil];
+    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:[NSURL URLWithString:@"http://cdn.tutsplus.com/mobile/uploads/2013/12/sample.jpg"]];
+    [downloadTask resume];
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location {
+    NSData *data = [NSData dataWithContentsOfURL:location];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.progress setHidden:YES];
+        [self.image setImage:[UIImage imageWithData:data]];
+    });
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didResumeAtOffset:(int64_t)fileOffset expectedTotalBytes:(int64_t)expectedTotalBytes {
+    
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite {
+    float progress = (double)totalBytesWritten / (double)totalBytesExpectedToWrite;
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.progress setProgress:progress];
+    });
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
